9e56212d9fc71b392a6d48e24dc3f3073a73e4be
Chris Chalmers <chris.chalmers@it-consulting.co>
2020-06-08 21:11:56 +0100

    Removed whitespace

--- global_vars.tf
+++ global_vars.tf
@@ -1,24 +1,24 @@
 variable "resource_location" {
-    type    = string
-    default = "UK South"
+  type    = string
+  default = "UK South"
 }
 
 variable "resource_prefix" {
-    type    = string
-    default = "CC-P-UKS"
+  type    = string
+  default = "CC-P-UKS"
 }
 
 variable "remote_location" {
-    type    = string
-    default = ""
+  type    = string
+  default = ""
 }
 
 variable "admin_username" {
-    type    = string
-    default = ""
+  type    = string
+  default = ""
 }
 
 variable "admin_password" {
-    type    = string
-    default = ""
+  type    = string
+  default = ""
 }
